bfe76c8f222b841f9e356de4bd509903cdd999dd
Zach Harrison <zharrison100@gmail.com>
2021-02-04 01:54:55 +0000

    Added and styled counts for products and categories in the database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
-  def show
     http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  def show
+    @product_count = Product.all.size
+    @category_count = Category.all.size
   end
 end
