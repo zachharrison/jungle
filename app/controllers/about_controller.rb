d275d860a8c42951595790694ee2c8d88a3a076d
Zach Harrison <zharrison100@gmail.com>
2021-02-04 03:25:28 +0000

    Working on routing bug

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+  def index
+    render 'index'
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  resource :about
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  resource :about
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
